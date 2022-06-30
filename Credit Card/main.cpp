1af1eb1ee73ca3800205fe1331260f26d73cad96
nerosighted <94204807+nerosighted@users.noreply.github.com>
2022-06-29 18:42:59 -0700

    Initial Commit

---
+++ Credit Card/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Credit Card
+//
+//  Created by Christian Millar on 6/29/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
